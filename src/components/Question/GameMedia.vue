048494e3921c0e6cf1ddd14b095342b8e59a7053
2 <okislev@gmail.com>
2022-07-26 11:54:38 +0300

    Support popcorn in vue context.

--- src/components/Question/GameMedia.vue
+++ src/components/Question/GameMedia.vue
@@ -4,6 +4,7 @@ import useGameManager from "src/utils/useGameManager";
 import useEmitter from "src/utils/useEmmiter";
 import ActionButton from "../ActionButton.vue";
 import AjaxViewer from "../AjaxViewer.vue";
+import { notificationDark } from "naive-ui";
 
 const gameManager = useGameManager();
 
@@ -41,8 +42,50 @@ function closeMedia(){
 }
 
 function editMediaForDevelopment(media){
-  return media.split('/static/').join('http://da-li.co.il/static/') //TODO update when production
+  var mymedia = media.split('/static/').join('http://da-li.co.il/static/') //TODO update when production
+  //return media.split('/static/').join('http://da-li.co.il/static/') //TODO update when production
+  var node = document.createElement("div");
+  node.innerHTML = media;
+
+  nodeScriptReplace(node);
+  return media
 }
+
+// TODO: move to utils
+// Parse scriupts within a node (question media) and create script elements
+// Taken from https://stackoverflow.com/questions/1197575/can-scripts-be-inserted-with-innerhtml
+function nodeScriptReplace(node,parentobj) 
+{
+  console.log('start');
+  if ( nodeScriptIs(node) === true ) {
+    console.log('found');
+    document.body.appendChild(nodeScriptClone(node));
+  }
+  else {
+    var i = -1, children = node.childNodes;
+    while ( ++i < children.length ) {
+      nodeScriptReplace( children[i] );
+    }
+  }
+
+  return node;
+}
+function nodeScriptClone(node){
+  var script  = document.createElement("script");
+  script.text = node.innerHTML;
+
+  var i = -1, attrs = node.attributes, attr;
+  while ( ++i < attrs.length ) {                                    
+    script.setAttribute( (attr = attrs[i]).name, attr.value );
+  }
+  return script;
+}
+
+function nodeScriptIs(node) {
+        return node.tagName === 'SCRIPT';
+}
+
+
 </script>
 
 
