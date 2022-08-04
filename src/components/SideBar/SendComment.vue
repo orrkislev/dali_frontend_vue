648c80e383779e9d73f5c4b3be91a386b8173a77
2 <okislev@gmail.com>
2022-08-04 18:13:13 +0300

    Send using post and fix res

--- src/components/SideBar/SendComment.vue
+++ src/components/SideBar/SendComment.vue
@@ -14,7 +14,7 @@ const gameManager = useGameManager()
 async function send_comment(){
     const sendData = { 'text':comment.value,'email':'','quest_id':''}
     if (gameManager.question && gameManager.question.q?.id) sendData.quest_id = gameManager.question.q.id
-    res = await api.post_json('review/comment/', sendData)
+    const res = await api.post('review/comment/', sendData)
     comment_sent.value = true
     setTimeout(()=>displayDialog.value = false ,500)
 }
