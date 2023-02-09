dacc64472b1e3d604d25525f1b6b97f743b04adf
2 <okislev@gmail.com>
2023-02-09 09:32:09 +0200

    Fixing youtube/popcorn video in game? Added key to ajaxviewer

--- src/components/Question/GameMedia.vue
+++ src/components/Question/GameMedia.vue
@@ -109,7 +109,7 @@ function nodeScriptIs(node) {
 
     <div v-if="gameManager.question.q.type != 'mouseselect'">
       <div v-for="(qmedia, i) in gameManager.question?.medias" :key="i" class='w-8 mx-auto media-area'>
-        <AjaxViewer :htmlWithScripts="editMediaForDevelopment(qmedia)" />
+        <AjaxViewer :htmlWithScripts="editMediaForDevelopment(qmedia)" :key="gameManager.question.q.id+'&'+i" />
       </div>
     </div>
   </div>
