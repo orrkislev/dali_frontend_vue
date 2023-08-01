b013d5af92f3754ed083000f9384c3b44c4eea51
2 <okislev@gmail.com>
2023-08-01 16:22:39 +0300

    Staff home buttons - mostly fixed

--- src/components/home/HomeStaff.vue
+++ src/components/home/HomeStaff.vue
@@ -2,13 +2,28 @@
 import { ref } from 'vue';
 import useAPI from '../../utils/useAPI';
 import AjaxViewer from '../AjaxViewer.vue';
+import { real_url } from "../../utils/useAPI";
 
 const api = useAPI()
 const htmlToShow = ref(null)
 
+const CALLS_PER_BUTTON={
+    'school report': 'statistics/schools_stat/',
+    'choose school': 'statistics/schools_stat/?purpose=list',
+    'school map' : 'static/pages/schools.htm',
+    'question report': 'statistics/questionstat/',
+    'question success': 'statistics/staff_quest_success/purpose=success',
+    'email teachers': 'review/select_autoemail/',
+    'signed teachers' : 'statistics/schools_stat/?purpose=CRM',
+    'purchased no teachers' :  'statistics/schools_stat/?purpose=CRM_no_teachers',  
+}
 async function click(btn){
     console.log(btn) 
-    if (btn == 'school report') htmlToShow.value = await api.post('statistics/schools_report/', {})
+    let url = real_url + CALLS_PER_BUTTON[btn]
+    window.open(url,'_target')
+    /*
+
+    if (btn == 'school report') htmlToShow.value = await api.post('statistics/schools_stat/', {})
     if (btn == 'choose school') htmlToShow.value = await api.post('statistics/schools_stat/', {purpose:'list'})
     if (btn == 'school map') window.open('/static/pages/schools.htm','_blank')
     if (btn == 'question report') htmlToShow.value = await api.post('statistics/questionstat/', {})
@@ -16,6 +31,7 @@ async function click(btn){
     if (btn == 'email teachers') htmlToShow.value = await api.post('review/select_autoemail/', {})
     if (btn == 'signed teachers') htmlToShow.value = await api.post('statistics/schools_stat/', {purpose:'CRM'})
     if (btn == 'purchased no teachers') htmlToShow.value = await api.post('statistics/schools_stat/', {purpose:'CRM_no_teachers'})
+    */
 }
 </script>
 
@@ -30,7 +46,7 @@ async function click(btn){
         <Button @click="()=>click('email teachers')" class="col-3">מייל למורים</Button>
         <Button @click="()=>click('signed teachers')" class="col-3">מורים נרשמו</Button>
         <Button @click="()=>click('purchased no teachers')" class="col-3">רכשו ולא נרשמו</Button>
-        <AjaxViewer v-if="htmlToShow" :htmlWithScripts="htmlToShow" />
+        <!--<AjaxViewer v-if="htmlToShow" :htmlWithScripts="htmlToShow" />-->
     </div>
 </template>
 
