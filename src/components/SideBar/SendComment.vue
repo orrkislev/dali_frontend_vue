3ec34ba59ea1fae8b96353592d063f9ea5ec83d9
2 <okislev@gmail.com>
2023-08-02 19:35:07 +0300

    הערה למנהלי האתר לא מתאפסת

--- src/components/SideBar/SendComment.vue
+++ src/components/SideBar/SendComment.vue
@@ -20,6 +20,7 @@ async function send_comment(){
     if (gameManager.question && gameManager.question.q?.id) sendData.question_id = gameManager.question.q.id
     const res = await api.post('review/comment/', sendData)
     comment_sent.value = true
+    comment.value = ""
     setTimeout(()=>displayDialog.value = false ,500)
 }
 </script>
