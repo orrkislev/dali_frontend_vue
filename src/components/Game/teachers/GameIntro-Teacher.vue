d0f8988a9e3cf6c3b81465c0a6a021e03cb844d0
2 <okislev@gmail.com>
2023-08-21 09:52:07 +0300

    Remove most of teacher's stuff from task page

--- src/components/Game/teachers/GameIntro-Teacher.vue
+++ src/components/Game/teachers/GameIntro-Teacher.vue
@@ -4,19 +4,25 @@ import ActionButton from '../../ActionButton.vue';
 import GameManageClasses from './GameManageClasses.vue';
 import Divider from 'primevue/divider';
 import TeacherActionSection from "../../TeacherActionSection.vue";
+import useGameManager from "../../../utils/useGameManager";
 
+const gameManager = useGameManager();
 const auth = useAuth();
 
 function startMobile(){
+  alert('not implemented yet')
   // TODO
 }
 function setExam(){
+  alert('not implemented yet')
   // TODO
 }
 function printExam(){
+  alert('not implemented yet')
   // TODO
 }
 function additional(){
+  alert('not implemented yet')
   // TODO
 }
 
@@ -30,13 +36,12 @@ function additional(){
     <Divider />
     <game-manage-classes />
     <Divider />
-
+    <div v-if="!gameManager.isLashon">
     <TeacherActionSection 
         title="משחק מובייל בכיתה" 
         subtitle="פתיחת המשחק למובייל בכיתה"
         text="בחרו את הכיתה/ות שבה/ן תרצו להתחיל עכשיו משחק בכיתה, בו המורה מציגה את השאלות במחשב, והתלמידים עונים בטלפונים הניידים."
         action_label="משחק מובייל"
-        :disabled="true"
         @click="startMobile" />
 
     <TeacherActionSection 
@@ -46,7 +51,6 @@ function additional(){
           לשחק רק פעם אחת, ללא גלגלי הצלה, והניקוד שקיבלו יופיע בדו"ח בחנים ללא
           צורך בפרסום. זכרו לסגור את הבוחן עם תום הזמן המוקצב לתלמידים.'
         action_label="בוחן"
-        :disabled="true"
         @click="setExam" />
     
     <TeacherActionSection 
@@ -54,7 +58,6 @@ function additional(){
         subtitle="מערכת דע-לי מייצרת עבורכם, באופן אוטומטי, קובץ בחינה אותו ניתן להורידולהדפיס."
         text="תוכלו לתת לתלמידים לתרגל את הנושא במערכת ולאחר מכן - לתת להם בוחן קצר המכיל מספר קטן של שאלות מהמאגר."
         action_label="הפקת בוחן"
-        :disabled="true"
         @click="printExam" />
 
     <TeacherActionSection 
@@ -64,6 +67,7 @@ function additional(){
         action_label="חומרי עזר"
         :disabled="true"
         @click="additional" />
+      </div>
   </div>
 </template>
 
