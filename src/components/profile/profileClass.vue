cf11ace3ac815dad2a33f8c3f3022d69e837101e
2 <okislev@gmail.com>
2023-08-31 14:37:56 +0300

    Show select class automatically if no class (of this year) selected

--- src/components/profile/profileClass.vue
+++ src/components/profile/profileClass.vue
@@ -12,7 +12,16 @@ const props = defineProps({
     edit: {type:Boolean, default: false},
     button: {type:Boolean, default: false},
     inedit: {type:Boolean, default: false},
+    autoStart: {type:Boolean, default: false},
+    reason : {type:String, default: ''},
 })
+
+
+const TEXT_PER_REASON = {
+  'not_slelected': 'עדיין לא נרשמת לכיתה. <br> עד שלא תירשמו לכיתה, לא תוכלו לקבל משימות מהמורה או לפרסם למורה הצלחה במשימות.<br>',
+  'not_updated' : '  ברוכים הבאים חזרה לאתר דע-לי.<br>כדי לקבל משימות מהמורה, יש לעדכן את קבוצת הלימוד.<br>',
+  '': 'בחרו כיתה מרשימת הכיתות של בית הספר:',
+}
 
 const schoolClasses = ref(null);
 const newId = ref(null);
@@ -21,29 +30,35 @@ async function getClassesList(){
   schoolClasses.value  = res.schoolClasses
 }
 
+if (props.autoStart) {
+  getClassesList()
+}
+
 </script>
 <template>
     <div class="flex p-2">
-      <div class="titleValueDiv">
+      <div v-if="!autoStart" class="titleValueDiv">
         <div class="w-4 mybold">{{ name }}:</div>
         <span style="margin-left:1em;">{{ val }}</span>
       </div>
       <div class="buttonsDiv">
-        <ButtonSmall v-if="button && !inedit" @click="inedit=true;getClassesList()" :primary="true" :border="true" :selected="true">שינוי</ButtonSmall>
-        <ButtonSmall v-if="inedit" @click="edit=false;inedit=false;newVal=val;schoolClasses=null" :primary="true" :border="true" :selected="true">ביטול</ButtonSmall>
+        <ButtonSmall v-if="button && !inedit && !autoStart" @click="inedit=true;getClassesList()" :primary="true" :border="true" :selected="true">שינוי</ButtonSmall>
+        <ButtonSmall v-if="inedit && !autoStart" @click="edit=false;inedit=false;newVal=val;schoolClasses=null" :primary="true" :border="true" :selected="true">ביטול</ButtonSmall>
       </div>  
       
     </div>
     <div v-if="schoolClasses && inedit" class="changeClassDiv">
         <br/>
-        <span class="instructionText"> בחרו כיתה מרשימת הכיתות של בית הספר:</span><br/><br/>
+        <span class="instructionText" v-html="TEXT_PER_REASON[reason]"> </span><br/><br/>
         <div v-for="sc in schoolClasses">
           <input  type='radio' name="newId" :value="sc.id" :checked="sc.name==val" @click="newId=sc.id"/>{{ sc.name }}<br/>
         </div>
         <br/>
-        אם הכיתה עדיין לא ברשימה - אנא פנו למורה.<br/>
+        אם הכיתה עדיין לא ברשימה - אנא פנו למורה.<br/><br/>
         <ButtonSmall v-if="newId" @click="emit('change', newId);inedit=false;newId=null" :primary="true" :border="true" >שמירה</ButtonSmall>
-      </div>
+        <ButtonSmall v-if="autoStart" @click="emit('leave')" :primary="true" :border="true" >סגירה ללא עדכון</ButtonSmall>
+    </div>
+    
 </template>
 
 <script>
